dbb6431bf45f675da342fcdacbb18674ed22152a
Roman Sidash <sidash.roman@mail.ru>
2022-05-20 23:16:26 +0600

    feat: reverse string

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,19 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	n := 0
+	rune := make([]rune, len(input))
+	for _, r := range input {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+
+	output = string(rune)
+
 	return output
 }
